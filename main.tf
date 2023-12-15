2331d7eec058bc96ff76a6238b269c8ce1f24000
veneraarykova <144861837+veneraarykova@users.noreply.github.com>
2023-12-14 18:42:14 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "web" {
+  ami = "ami-048e636f368eb3006"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  key_name = "my laptop key"
+}
