2b239acdbcf69620c5d4208f1c9f850743026a37
veneraarykova <144861837+veneraarykova@users.noreply.github.com>
2023-12-14 20:02:09 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 }
 resource "aws_instance" "web" {
-  ami = "ami-048e636f368eb3006"
-  instance_type = "t2.micro"
-  availability_zone = "us-east-2a"
-  key_name = "my laptop key"
+  ami = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.az
+  key_name = var.key
 }
