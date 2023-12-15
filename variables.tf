2f5557f8c673aeb4c7a0e6df8fdad39808c1dc55
veneraarykova <144861837+veneraarykova@users.noreply.github.com>
2023-12-14 20:03:21 -0600

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable region {
+    type = string
+    default = ""
+}
+
+variable ami_id {
+    type = string
+    default = ""
+}
+
+variable instance_type {
+    type = string
+    default = ""
+}
+
+variable az {
+    type = string
+    default = ""
+}
+
+variable key {
+    type = string
+    default = ""
+}
