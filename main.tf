3d38207f10e39f984661286347b1f649612a141a
veneraarykova <144861837+veneraarykova@users.noreply.github.com>
2023-12-19 20:17:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,17 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "web" {
-  ami = var.ami_id
+  ami = data.aws_ami.amazon.id
   instance_type = var.instance_type
   availability_zone = var.az
   key_name = var.key
+
+data "aws_ami" "amazon" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+  owners = ["self"]
+}
 }
